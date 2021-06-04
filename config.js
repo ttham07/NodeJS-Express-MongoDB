f26edf5c1a1953a35fcd9af5e39f5776d145be74
Tommy Tham <tommytham@theShopMBPro.attlocal.net>
2021-06-04 09:58:33 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '1130976414069262',
+        clientSecret: '5e1499884cef9b7df4a7e85ea87dfbc1'
+    }
 }
